2bcd0d5e48f7a1697d3bc938024b3da7e768bcdd
saanyrock <saanyrock@gmail.com>
2016-06-13 14:05:26 -0300

    Abas

--- paginas/produtos.php
+++ paginas/produtos.php
@@ -1,16 +1,13 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <title>TODO supply a title</title>
-        <meta charset="windows-1252">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    </head>
-    <body>
-        <div>TODO write content</div>
-    </body>
-</html>
+<div class="conteudo">
+    
+    <div class="abas">
+        <div class="aba">
+             <h1>PRODUTOS</h1>
+        </div>
+
+        <div class="aba">
+             <h1>CUIDADOS</h1>
+        </div>
+    </div>
+    
+</div>
