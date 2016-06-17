e006a2e513ebdc40351a9369f228381d6a0821d3
saanyrock <saanyrock@gmail.com>
2016-06-17 01:14:40 -0300

    responsivo um

--- index.php
+++ index.php
@@ -10,11 +10,11 @@
     <link rel="stylesheet" href="css/estilo.css"/>
     <script type="text/javascript" src="js/jquery-2.2.3.min.js"></script>
     <script type="text/javascript" src="js/menu.js"></script>
+    <script type="text/javascript" src="js/responsivo.js"></script>
 
 </head>
-<body>
 
-<body onload="menuativo()">
+<body onload="menuativo(); responsive()" onresize="responsive()">
     <header>
         <div class="midia">
             <a class="face" href="https://www.facebook.com/dasmariassemijoias" target="_blank">
@@ -24,40 +24,41 @@
                 <img src="img/insta.png">
             </a>
         </div>
-        
-        <div class="logo"><a " href="?pagina=home"><a " href="?pagina=home"><a " href="?pagina=home"></a><a " href="?pagina=home"><img src="img/logo.png"/></a></a></a></div>
-        
-<a href='#' class="menu-compacto-trigger" onclick="mudarMenuCompacto()">
-                <img src='img/diamante-08.png' width='50'/>
-            </a>
-        <menu id="menu-compacto">
-            <div class="menuItem" name="home"><a class="menuLink" href="?pagina=home">Home</a></div>
-            <div class="menuItem" name="empresa"><a class="menuLink" href="?pagina=empresa">Empresa</a></div>
-            <div class="menuItem" name="produtos"><a class="menuLink" href="?pagina=produtos">Produtos</a></div>
-            <div class="menuItem" name="vendas"><a class="menuLink" href="?pagina=vendas">Vendas</a></div>
-            <div class="menuItem" name="contato"><a class="menuLink" href="?pagina=contato">Contatos</a></div>
+
+        <menu id="menu-cabecalho">
+            <div class="menuItem" name="home" id="home"><a class="menuLink" href="?pagina=home">Home</a></div>
+            <div class="menuItem" name="empresa" id="empresa"><a class="menuLink" href="?pagina=empresa">Empresa</a></div>
+            <div class="menuItem" name="produtos" id="produtos"><a class="menuLink" href="?pagina=produtos">Produtos</a></div>
+            <div class="menuItem" name="vendas" id="vendas"><a class="menuLink" href="?pagina=vendas">Vendas</a></div>
+            <div class="menuItem" name="contato" id="contato"><a class="menuLink" href="?pagina=contato">Contatos</a></div>
             </div>
         </menu>
+        <div class="logo"><a  href="?pagina=home"><a  href="?pagina=home"><a  href="?pagina=home"></a><a  href="?pagina=home"><img src="img/logo.png"/></a></a></a></div>
 
-    </header>
 
-    <main>
-        <?php
-        if (isset($_GET['pagina'])) {
-            include "paginas/" . $_GET['pagina'] . ".php";
-        } else {
 
-            include "paginas/home.php";
-        }
-        ?>
-    </main>
 
-    <footer>           
-        <p>
-            (62) 3637-2142<br>
-            R. Benjamin Constant, 674, Goiânia - GO, 74525-050               
-        </p>
-        <div id="copyright">Copyright © 2016 - Design Grafico Modulo III Senac-GO | Todos os direitos reservados e protegidos</div>
-    </footer>
+    </menu>
+
+</header>
+
+<main>
+    <?php
+    if (isset($_GET['pagina'])) {
+        include "paginas/" . $_GET['pagina'] . ".php";
+    } else {
+
+        include "paginas/home.php";
+    }
+    ?>
+</main>
+
+<footer>           
+    <p>
+        (62) 3637-2142<br>
+        R. Benjamin Constant, 674, Goiânia - GO, 74525-050               
+    </p>
+    <div id="copyright">Copyright © 2016 - Design Grafico Modulo III Senac-GO | Todos os direitos reservados e protegidos</div>
+</footer>
 </body>
 </html>
